30e0cf6059f19ae151b179fc93db1e135667f207
rrs12 <rahilsalecha12@gmail.com>
2024-04-28 15:56:47 +0530

    update

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,11 @@
 const navLinks = document.querySelector('.nav-links')
 const togglebtn= document.querySelector('.toggle-btn')
+        function onToggleMenu(e){
+            e.name = e.name === 'menu' ? 'close' : 'menu'
+            navLinks.classList.toggle('hidden')
+
+        }
+
 function hide(){
     navLinks.classList.toggle('hidden')
     togglebtn.name = togglebtn.name === 'menu' ? 'close' : 'menu'
